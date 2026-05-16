68f0e251c8da47ff14deac6bdacf5e0b0fbe0507
Olena Iliushyn <lenailyshun@gmail.com>
2026-05-16 12:28:22 +0300

    Overload operators

---
+++ 07_Exeptions/Program.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Runtime.Serialization;
+
+namespace _07_Exeptions
+{
+    class MyClass
+    {
+        public void BadMethod()
+        {
+            Exception exception = new Exception("My Exeption message");
+            exception.HelpLink = "https://google.com";
+            exception.Data.Add("Exception reason : ", "Test system");
+            exception.Data.Add("Time invocation : ", DateTime.Now);
+            exception.Data.Add("User name : ", "Olena");
+            throw exception;
+        }
+    }
+    class LoginExeption : Exception
+    {
+        public DateTime Time { get; set; } = DateTime.Now;
+        public LoginExeption()
+        {
+           //Time = DateTime.Now;
+        }
+
+        public LoginExeption(string? message) : base(message)
+        {
+            //Time = DateTime.Now;
+        }
+
+        public LoginExeption(string? message, Exception? innerException) : base(message, innerException)
+        {
+            //Time = DateTime.Now;
+        }
+
+        protected LoginExeption(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+    class User
+    {
+        private string login;
+        public string Login
+        {
+            get { return login; }
+            set {
+                if (value == "")
+                    throw new LoginExeption("Login is invalid...");
+                else
+                    login = value;
+            }
+        }
+    }
+    class UserException : Exception
+    {
+        public void Method()
+        {
+            Console.WriteLine("My Exception!");
+        }
+    }
+    internal class Program
+    {
+        static double SafeDivision(double a, double b)
+        {
+            if( b == 0)
+            {
+                throw new DivideByZeroException("Second operand = 0");
+            }
+            return a / b;   
+        }
+        static void Main(string[] args)
+        {
+            #region Example 1
+            /*
+            int a = 1, b = 2;
+
+            //string str = null;
+            //str.ToUpper();
+
+            //try catch finally  throw
+            try
+            {
+                Console.WriteLine("Enter number a : ");
+                //a = int.Parse(null);
+                a = int.Parse(Console.ReadLine()!);//""     null
+                Console.WriteLine("Enter number b : ");
+                b = int.Parse(Console.ReadLine()!);
+
+                a = a / b;
+                Console.WriteLine($"Res : {a}");
+
+            }
+            
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Can't divide by zero....Study Math!!!");
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("To large number");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("You must enter number");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("ArgumentNullException");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Continue.............");
+            Console.WriteLine("Continue.............");
+            Console.WriteLine("Continue.............");
+            Console.WriteLine("Continue.............");
+            */
+
+
+            #endregion
+            #region Example 2
+            /*
+            double a = 85, b = 0, res = 0;
+            try
+            {
+                res = SafeDivision(a, b);
+                Console.WriteLine(res);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Can't divide by zero....Study Math!!!");
+
+            }
+            */
+            #endregion
+            #region Example 3
+            /*
+            try
+            {
+                MyClass myClass = new MyClass();
+                myClass.BadMethod();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Member name : {e.TargetSite}");
+                Console.WriteLine($"Member class : {e.TargetSite.DeclaringType}");
+                Console.WriteLine($"Member type : {e.TargetSite.MemberType}");
+                Console.WriteLine($"Method name : {e.TargetSite.Name}");
+                Console.WriteLine($"Message : {e.Message}");
+                Console.WriteLine($"Source : {e.Source}");
+                Console.WriteLine($"Help Link : {e.HelpLink}");
+                Console.WriteLine($"Stack Trace : {e.StackTrace}");
+                foreach (DictionaryEntry dic in e.Data)
+                {
+                    Console.WriteLine($"{dic.Key} -- {dic.Value}");
+                }
+               
+            }
+            */
+            #endregion
+            #region Example 4
+            /*
+            User user = new User();
+
+            try
+            {
+                user.Login = "";
+            }
+            catch (LoginExeption ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Time);
+            }*/
+            #endregion
+            #region Example 5
+            try
+            {
+                throw new UserException();
+            }
+            catch (UserException userException)
+            {
+                Console.WriteLine("Exception catch!.");
+                userException.Method();
+                FileStream fs = null;
+                try
+                {
+                    fs = File.Open(@"C:\NonExistentFile.log", FileMode.Open);
+                    //write
+
+                    //....exception 
+
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+
+                }
+                finally
+                {
+                    //fs.Close();
+                    Console.WriteLine("End inner try");
+                }
+
+            }
+            finally
+            {
+                Console.WriteLine("End outer try");
+            }
+            #endregion
+        }
+    }
+}
---
+++ 08_OverloadOperators/Program.cs
@@ -0,0 +1,227 @@
+﻿namespace _08_OverloadOperators
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point() : this(0, 0) { }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return $"X : {X}. Y : {Y}";
+        }
+        //public static return_type operator [symbol](parameters)
+        //{  code.... }
+        #region Uno operators  - (-5) ++ --
+        public static Point operator - (Point p)
+        {
+            Point res = new Point() 
+            { 
+               X = -p.X,
+               Y = -p.Y,
+            };
+           
+            return res ;
+        }
+        public static Point operator ++(Point p)
+        {
+            p.X++;
+            p.Y++;
+            return p;
+        }
+        public static Point operator --(Point p)
+        {
+            p.X--;
+            p.Y--;
+            return p;
+        }
+        #endregion
+        #region Binary operators + - * /
+        public static Point operator +(Point p1, Point p2)
+        {
+            Point res = new Point()
+            {
+                X = p1.X + p2.X,
+                Y = p1.Y + p2.Y,
+            };
+            return res;
+        }
+        public static Point operator -(Point p1, Point p2)
+        {
+            Point res = new Point()
+            {
+                X = p1.X - p2.X,
+                Y = p1.Y - p2.Y,
+            };
+            return res;
+        }
+        public static Point operator *(Point p1, Point p2)
+        {
+            Point res = new Point()
+            {
+                X = p1.X * p2.X,
+                Y = p1.Y * p2.Y,
+            };
+            return res;
+        }
+        public static Point operator /(Point p1, Point p2)
+        {
+            Point res = new Point()
+            {
+                X = p1.X / p2.X,
+                Y = p1.Y / p2.Y,
+            };
+            return res;
+        }
+
+        #endregion
+        #region Logic operators
+        public static bool operator ==(Point p1, Point p2)
+        {
+            return p1.X== p2.X && p1.Y == p2.Y;  
+        }
+        //in  pair
+        public static bool operator !=(Point p1, Point p2)
+        {
+            return p1.X != p2.X || p1.Y != p2.Y;
+        }
+
+        public static bool operator >(Point p1, Point p2)
+        {
+            return (p1.X +  p1.Y ) > (p2.X + p2.Y);
+        }
+        //in pair
+        public static bool operator <(Point p1, Point p2)
+        {
+            return (p1.X + p1.Y) < (p2.X + p2.Y);
+        }
+        public static bool operator >=(Point p1, Point p2)
+        {
+            return (p1.X + p1.Y) >= (p2.X + p2.Y);
+        }
+        //in pair
+        public static bool operator <=(Point p1, Point p2)
+        {
+            return (p1.X + p1.Y) <= (p2.X + p2.Y);
+        }
+        #endregion
+        #region true/false operators
+        public static bool operator true(Point p1)
+        {
+            return (p1.X > 0 && p1.Y > 0);    
+        }
+        //in pair
+        public static bool operator false(Point p1)
+        {
+            return p1.X < 0 || p1.Y < 0;
+        }
+
+        #endregion
+        #region Overload types
+        public static explicit operator int(Point p1)
+        {
+            return p1.X + p1.Y;
+        }
+        //public static implicit operator int(Point p1)
+        //{
+        //    return p1.X + p1.Y;
+        //}
+        public static implicit operator Point_3D(Point p1)
+        {
+            return new Point_3D(p1.X, p1.Y, p1.X+p1.Y);
+        }
+
+
+        #endregion
+    }
+    class Point_3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public Point_3D() : this(0, 0,0) { }
+        public Point_3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public override string ToString()
+        {
+            return $"X : {X}. Y : {Y}. Z : {Z}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            int a = 5;//int --> int
+            double d = 3.6;//double ---> double
+
+            d = a;//int ---> double 5.0000000000000000 (IMPLICIT)   
+            a = (int)d;//double ----> int 3  explicit
+
+
+            int b = 4;
+            Console.WriteLine(a+b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+
+            Point p1 = new Point(5, 5);
+            Console.WriteLine("------------------Overload int");
+            a = (int)p1;
+            Console.WriteLine(a);
+
+            Point_3D p3 = p1;
+            Console.WriteLine(p3);
+
+            Point p2 = new Point(7, 4);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p1++);
+            Console.WriteLine(++p1);
+            Console.WriteLine(p1--);
+            Console.WriteLine(--p1);
+            Console.WriteLine(p1+p2);
+            Console.WriteLine(p1-p2);
+            Console.WriteLine(p1*p2);
+            Console.WriteLine(p1/p2);
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            if ( p1 == p2)
+            {
+                Console.WriteLine("Equals");
+            }
+            else
+            {
+                Console.WriteLine("Not equals");
+            }
+            if (p1 > p2)
+            {
+                Console.WriteLine("p1 > p2");
+            }
+            else
+            {
+                Console.WriteLine("p1 < p2");
+            }
+            if (p1)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
