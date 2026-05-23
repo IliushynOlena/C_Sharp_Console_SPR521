f09e8713640e0c044957eb89e7b258d595d458a0
Olena Iliushyn <lenailyshun@gmail.com>
2026-05-23 11:43:27 +0300

    Inheritance, Indexers

---
+++ 09_Inheritance/Program.cs
@@ -0,0 +1,164 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CSharp;
+
+ 
+abstract class  Person// : Object
+{
+    protected string name;
+    private readonly DateTime birthdate;
+    public Person()
+    {
+        name = "no name";
+        birthdate = new DateTime();//01.01.0001
+    }
+    public Person(string n, DateTime b)
+    {
+        name = n;
+        if( b > DateTime.Now)
+        {
+            birthdate = new DateTime();
+        }
+        else
+        {
+            birthdate = b;
+        }
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine($"Name : {name}. " +
+            $"Birth : {birthdate.ToShortDateString()}");
+    }
+    public abstract void DoWork();
+    public override string ToString()
+    {
+        return $"Name : {name}. " +
+            $"Birth : {birthdate.ToShortDateString()}";
+    }
+}
+//class Child : BaseClass, Interface1, Interface2, Interface3
+class Worker :  Person // public 
+{
+    private decimal salary;
+
+    public decimal Salary
+    {
+        get { return salary; }
+        set { this.salary = value > 0 ? value : 0; }
+    }
+    public Worker(): base()
+    {
+        salary = 0;
+    }
+    public Worker(string n, DateTime b, decimal s):base(n, b) 
+    {    
+        Salary = s;
+    }
+    public override void DoWork()
+    {
+        Console.WriteLine("Doing some work!!!!");
+    }
+    //new or override
+    public override void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Salary : {Salary}");
+    }
+}
+sealed class Programmer : Worker
+{
+    public int CodeLines { get; set; }
+    public Programmer(): base()
+    {
+           CodeLines = 0; 
+    }
+    public Programmer(string n, DateTime b, decimal s): base(n,b,s)
+    {
+        CodeLines = 0;
+    }
+    public sealed override void DoWork()
+    {
+        Console.WriteLine("Write C# code!");
+    }
+    public void WriteCodeLines()
+    {
+        CodeLines++;
+    }
+    public override void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Write code lines : {CodeLines}");
+    }
+}
+
+//sealed - метод заборонений до перевизначення 
+class TeamLead : Worker
+{
+    public int ProjectCount { get; set; }
+    //public override void DoWork()
+    //{
+    //    Console.WriteLine("Manage Team projects");
+    //}
+}
+
+class Program
+{
+    static void Main()
+    {
+        Worker worker = new Worker("Misha", new DateTime(2000,12,5), 15000);
+        worker.Print();
+
+        Programmer pr = new Programmer("Petya",new DateTime(1999,5,5), 45000);
+        pr.Print();
+        pr.WriteCodeLines();
+        pr.WriteCodeLines();
+        pr.WriteCodeLines();
+        pr.Print();
+
+        Person[] people = new Person[]
+        {
+            //new Person(),
+            worker,
+            new Programmer("Petya",new DateTime(1999,5,5), 45000)
+        };
+        //virtual override
+        foreach (var person in people)
+        {
+            Console.WriteLine("-------------Info----------------");
+            person.Print();
+            Console.WriteLine();
+            person.DoWork();
+        }
+
+        Programmer p = null;
+
+        //------------ 1 - --- use cast ()
+
+        try
+        {
+            p = (Programmer)people[0];
+            p.DoWork();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        //--------------- 2 ----    use as
+        p = people[0] as Programmer;
+        if (p == null)
+            Console.WriteLine("reference empty");
+        else
+            p.DoWork();
+
+        //--------------3- --  use IS and AS
+
+        if (people[1] is Programmer)
+        {
+            p = people[1] as Programmer;
+            p.DoWork();
+        }
+        else Console.WriteLine("Incorrect type");
+
+    }
+}
---
+++ 10_Indexers/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace _10_Indexers
+{
+    class Laptop
+    {
+        public string Mark { get; set; }
+        public double Price { get; set; }
+        public override string ToString()
+        {
+            return $"Mark : {Mark}. Price : {Price}";
+        }
+    }
+    class Shop
+    {
+        private Laptop[] laptops;
+        public Shop(int size)//5
+        {
+            laptops = new Laptop[size];
+        }
+        public int Length
+        {
+            get { return laptops.Length; }
+        }
+        public Laptop GetLaptop(int index)//2000
+        {
+            if (index >= 0 && index < laptops.Length)
+            {
+                return laptops[index];
+            }
+            else
+                throw new IndexOutOfRangeException();
+
+        }
+
+        public void SetLaptop(Laptop laptop, int index)
+        {
+            if (index >= 0 && index < laptops.Length)
+            {
+                laptops[index] = laptop;
+            }
+            else
+                throw new IndexOutOfRangeException();
+        }
+        public Laptop this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < laptops.Length)
+                {
+                    return laptops[index];
+                }
+                else
+                    throw new IndexOutOfRangeException();
+            }
+            set //VALUE
+            {
+                if (index >= 0 && index < laptops.Length)
+                {
+                    laptops[index] = value;
+                }
+                else
+                    throw new IndexOutOfRangeException();
+            }
+        }
+        public Laptop this[string name]//HP
+        {
+            get
+            {
+                //readonly
+                foreach (var laptop in laptops)
+                {
+                    if (laptop.Mark == name)
+                        return laptop;
+                }
+                return null;
+            }
+            //private set //value
+            //{
+            //    for (int i = 0; i < laptops.Length; i++)
+            //    {
+            //        if(laptops[i].Mark == name)
+            //        {
+            //            laptops[i] = value;
+            //            break;
+            //        }
+            //    }
+
+            //}
+        }
+        public int FindByPrice(double price)
+        {
+            for (int i = 0; i < laptops.Length; i++)
+            {
+                if (laptops[i].Price == price) return i;
+
+            }
+            return -1;
+        }
+        public Laptop this[double price]
+        {
+            get
+            {
+                int index = FindByPrice(price);
+                if (index != -1)
+                {
+                    return laptops[index];
+                }
+                throw new Exception("Inncorect price");
+            }
+            set
+            {
+                int index = FindByPrice(price);
+                if (index != -1)
+                {
+                    this[index] = value;
+                }
+            }
+        }
+    }
+    public class MultArray
+    {
+        private int[,] array;
+        public int Rows { get; private set; }
+        public int Cols { get; private set; }
+        public MultArray(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            array = new int[rows, cols];
+        }
+        public int this[int r, int c]
+        {
+            get { return array[r, c]; }
+            set { array[r, c] = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MultArray multArray = new MultArray(2, 3);
+
+            //for (int i = 0; i < multArray.Rows; i++)
+            //{
+            //    for (int j = 0; j < multArray.Cols; j++)
+            //    {
+
+            //        multArray[i, j] = i + j;//indexator - set
+            //        Console.Write($"{multArray[i, j]} ");//indexator - get
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            Shop shop = new Shop(3);
+            shop.SetLaptop(new Laptop() { Mark = "HP", Price = 25000 }, 0);
+            Console.WriteLine(shop.GetLaptop(0));
+
+            shop[1] = new Laptop() { Mark = "Asus", Price = 35000 };
+            Console.WriteLine(shop[1]); 
+            int[] arr = new int[3];
+            arr[0] = 100;
+            Console.WriteLine(arr[0]);
+            Console.WriteLine("-------------- [string name ]----------");
+            //shop["HP"] = new Laptop() { Mark = "Mac", Price = 100000 };
+            Console.WriteLine(shop["HP"]);
+
+            shop[25000.0] = new Laptop() { Mark = "HP", Price = 19999 };
+            for (int i = 0; i < shop.Length; i++)
+            {
+                Console.WriteLine(shop[i]);
+            }
+            Console.WriteLine(shop[(double)19999]);
+        }
+    }
+}
